647d3dd5d9fc6fb3befa6ffdde4a6673bc2264c1
Ayush <ayushvsnsvm9101112@gmail.com>
2020-04-01 00:10:58 +0530

    problem3 added

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -5,6 +5,37 @@
  *  Time: 7:13 PM
  */
 package problem3.node;
+import problem5.student.Student;
 
-public class Node {
+public class Node{
+    private Node next;
+    private Student student;
+    private int priority;
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
 }
+
+
